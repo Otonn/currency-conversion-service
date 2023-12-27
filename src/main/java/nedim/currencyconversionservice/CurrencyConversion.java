8494191584297ed2339c1471038ddf5d85396997
Nedim <nedim1jukic@gmail.com>
2023-12-27 22:39:13 +0100

    Initial commit

---
+++ src/main/java/nedim/currencyconversionservice/CurrencyConversion.java
@@ -0,0 +1,21 @@
+package nedim.currencyconversionservice;
+
+
+import lombok.*;
+
+import java.math.BigDecimal;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CurrencyConversion {
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal quantity;
+    private BigDecimal conversionMultiple;
+    private BigDecimal totalCalculatedAmount;
+    private String environment;
+}
